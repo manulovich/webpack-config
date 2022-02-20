e5eefbd2424f48159d0ab21e9c71e1461d587d3b
Влад Котельников <manulovich@iMac-Vlad.local>
2022-02-20 10:27:56 +0300

    basic config

---
+++ README.md
@@ -0,0 +1,15 @@
+# Webpack config
+
+Простой и пока не рабочий конфиг для верстки:(
+
+## Начало работа
+
+Клонируем
+```bash
+git clone https://github.com/manulovich/webpack-config.git
+```
+
+Устанавливаем зависимости
+```
+npm i
+```
---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    context: path.resolve(__dirname, 'src'),
+    entry: {
+        app: ['@babel/polyfill', './scripts/app.js']
+    },
+    output: {
+        filename: '[name].js', // '[name].[contenthash].js'
+        path: path.resolve(__dirname, 'dist'),
+        clean: true
+    },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'dist'),
+        },
+        compress: true,
+        port: 3000,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'style.css'
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                    from: './assets',
+                    to: path.resolve(__dirname, './dist/assets')
+                }
+            ]
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader']
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    }
+}
